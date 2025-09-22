d34f14890cc21371514b0d083906ba61f75c3162
Pitz72 <pizzisimone1972@gmail.com>
2025-09-22 14:45:47 +0200

    Fix final TypeScript errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect } from 'react';
-import './App.css';
 import { useGameScale } from './hooks/useGameScale';
 import { usePlayerMovement } from './hooks/usePlayerMovement';
 import { useGameStore } from './stores/gameStore';
@@ -229,7 +228,7 @@ const GameContent = () => {
           onRemove={removeNotification} 
         />
         <div className="h-full flex flex-col">
-          <NarrativeManager />
+          <NarrativeManager isGameActive={currentScreen === 'game' && !isGameOver} />
           {combatResult ? (
             <PostCombatScreen
               result={combatResult}
